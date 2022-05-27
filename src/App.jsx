4c585659d0b1867d96c5b4f4d3adb344244dc5ac
Bevzyk SOI <bevzyk.soi@gmail.com>
2022-05-27 16:47:48 +0200

    Started practice

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { Component } from "react";
+import styles from "./App.module.css";
+
+export default class App extends Component {
+    render() {
+        return(
+            <h1 className={styles.mainHeading}>I HAVE A VERY BIG EXPERIENCE OF REACT!!! HELLO WORLD!!!</h1>
+        );
+    }
+}
