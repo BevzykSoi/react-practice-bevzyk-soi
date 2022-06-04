2f7bc1dc231aca1eac662cfa355f3c5fc4422d49
Bevzyk SOI <bevzyk.soi@gmail.com>
2022-06-04 13:25:37 +0200

    Tried to do homework

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,35 @@
-import { Component } from "react";
+import { useState, useRef } from "react";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import Sidebar from "./components/Sidebar/Sidebar";
 import styles from "./App.module.css";
-import logo from"./images/react-logo.png";
+import logo from "./images/react-logo.png";
 
-export default class App extends Component {
-    render() {
-        return (
-            <div className={styles.App}>
-                <Sidebar></Sidebar>
-                <Header></Header>
-                <h2 className={styles.desc}>IT IS MY REACT PRACTICE!!!</h2>
-                <img src={logo} alt="React logo" className={styles.logo}/>
-                <Footer></Footer>
-            </div>
-        );
+function App() {
+    let value = 2;
+
+    let [inputValArray, setInputValArray] = useState([]);
+    const inputRef = useRef();
+
+    function handleInput() {
+        setInputValArray(array => {
+            inputValArray = [...array, 1];
+            console.log(inputValArray);
+        });
     }
-}
+
+    return (
+        <div className={styles.App}>
+            <Sidebar></Sidebar>
+            <Header></Header>
+            <h2 className={styles.desc}>IT IS MY REACT PRACTICE!!!</h2>
+            <img src={logo} alt="React logo" className={styles.logo} />
+            {value <= 1 ? <div className={styles.black}></div> : <div className={styles.red}></div>}
+            <input type="text" ref={inputRef} />
+            <button type="button" onClick={handleInput}>Handle Input</button>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default App;
